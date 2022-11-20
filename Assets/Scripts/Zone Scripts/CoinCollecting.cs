d567fb6fda608a4c194df54954d38bdb770b1843
s0s100 <s0s100@mail.ru>
2022-11-20 15:55:30 +0000

    Coin implementation

--- Assets/Scripts/Zone Scripts/CoinCollecting.cs
+++ Assets/Scripts/Zone Scripts/CoinCollecting.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class CoinCollecting : MonoBehaviour
 {
+    private CoinController coinController;
+    private const float TIME_BEFORE_DELETE = 0.5f;
+    private Animator animator;
+
+    private void Start()
+    {
+        coinController = FindObjectOfType<CoinController>();
+        animator = this.GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject collidedObject = collision.gameObject;
         string objectTag = collidedObject.tag;
         if (objectTag == "Player")
         {
-            Destroy(this.gameObject);
+            animator.SetTrigger("IsTaken");
+            coinController.AddCoin();
+            Destroy(this.gameObject, TIME_BEFORE_DELETE);
         }
     }
 }
