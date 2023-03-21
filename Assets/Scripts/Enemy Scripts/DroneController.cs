405cee204765f56dd5813488fe083668b46b2d19
s0s100] <s0s100@mail.ru>
2023-03-21 12:37:29 +0000

    Minor drone update

--- Assets/Scripts/Enemy Scripts/DroneController.cs
+++ Assets/Scripts/Enemy Scripts/DroneController.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class DroneController : FlyingEnemy
 {
-    
+    //private const float REQUIRED_DISTANCE_BEFORE_PLAYER = 5.0f;
+
+    //private bool isMoving = true;
+
+    //protected override void Awake()
+    //{
+    //    MoveCloseToPlayer();
+    //}
+
+    //private void MoveCloseToPlayer()
+    //{
+
+    //}
+
+    //protected override void Update()
+    //{
+    //    DecideNextAction();
+    //    InvulnerabilityControl();
+    //}
+
+    //private void DecideNextAction()
+    //{
+
+    //}
 }
