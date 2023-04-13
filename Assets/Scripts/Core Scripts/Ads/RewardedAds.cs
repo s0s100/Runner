83c5bf93e9271fc2943f20a324ee50c8939987bf
s0s100] <s0s100@mail.ru>
2023-04-13 07:02:48 +0700

    Advertisement implementation

---
+++ Assets/Scripts/Core Scripts/Ads/RewardedAds.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.UI;
+
+public class RewardedAds : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    [SerializeField] 
+    private Button showAdButton;
+    [SerializeField] 
+    private string androidAdUnitId = "Rewarded_Android";
+    [SerializeField] 
+    private string iosAdUnitId = "Rewarded_iOS";
+
+    private string adUnitId = null;
+
+    void Awake()
+    {
+        defineCurUnitId();
+
+        //Disable the button until the ad is ready to show:
+        showAdButton.interactable = false;
+    }
+
+    private void defineCurUnitId()
+    {
+#if UNITY_IOS
+        adUnitId = iosAdUnitId;
+#elif UNITY_ANDROID
+        adUnitId = androidAdUnitId;
+#elif UNITY_EDITOR
+        gameId = iosAdUnitId; // For now
+#endif
+    }
+
+    // Load content to the Ad Unit:
+    public void LoadAd()
+    {
+        // IMPORTANT! Only load content AFTER initialization (in this example, initialization is handled in a different script).
+        Debug.Log("Loading Ad: " + adUnitId);
+        Advertisement.Load(adUnitId, this);
+    }
+
+    // If the ad successfully loads, add a listener to the button and enable it:
+    public void OnUnityAdsAdLoaded(string adUnitId)
+    {
+        Debug.Log("Ad Loaded: " + adUnitId);
+
+        if (adUnitId.Equals(this.adUnitId))
+        {
+            // Configure the button to call the ShowAd() method when clicked:
+            showAdButton.onClick.AddListener(this.ShowAd);
+            // Enable the button for users to click:
+            showAdButton.interactable = true;
+        }
+    }
+
+    // Implement a method to execute when the user clicks the button:
+    public void ShowAd()
+    {
+        // Disable the button:
+        showAdButton.interactable = false;
+        // Then show the ad:
+        Advertisement.Show(adUnitId, this);
+    }
+
+    // Implement the Show Listener's OnUnityAdsShowComplete callback method to determine if the user gets a reward:
+    public void OnUnityAdsShowComplete(string adUnitId, UnityAdsShowCompletionState showCompletionState)
+    {
+        if (adUnitId.Equals(this.adUnitId) && showCompletionState.Equals(UnityAdsShowCompletionState.COMPLETED))
+        {
+            Debug.Log("Unity Ads Rewarded Ad Completed");
+            // Grant a reward.
+        } else
+        {
+            Debug.Log("Unity Ads Rewarded Ad Is Not Completed");
+        }
+    }
+
+    // Implement Load and Show Listener error callbacks:
+    public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Error loading Ad Unit {adUnitId}: {error.ToString()} - {message}");
+        // Use the error details to determine whether to try to load another ad.
+    }
+
+    public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
+        // Use the error details to determine whether to try to load another ad.
+    }
+
+    public void OnUnityAdsShowStart(string adUnitId) { }
+    public void OnUnityAdsShowClick(string adUnitId) { }
+
+    void OnDestroy()
+    {
+        // Clean up the button listeners:
+        showAdButton.onClick.RemoveAllListeners();
+    }
+}
