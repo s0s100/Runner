41f6a246f0580e5dea6f604491797370c9004e56
s0s100] <s0s100@mail.ru>
2023-05-15 16:16:21 +0100

    Created parent powerup class

---
+++ Assets/Scripts/Prefab Scripts/PowerUps/PowerUp.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    private float creationTime;
+    private bool isDisappearing = false;
+    private float disappearanceSpeed;
+
+    [SerializeField]
+    private float disappearanceTime = 1.0f;
+    [SerializeField]
+    private float disappearanceMoveSpeed = 2.0f;
+    [SerializeField]
+    private GameObject background;
+    [SerializeField]
+    private float sinMoveSpeed = 1.0f;
+    [SerializeField]
+    private float sinCoef = 0.1f;
+    [SerializeField]
+    private float timeCoef = 2.0f;
+    [SerializeField]
+    private float sinTimeShift = 0.0f;
+
+    private void Awake()
+    {
+        creationTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (!isDisappearing)
+        {
+            VisualMovement();
+        } else
+        {
+            Disappearance();
+        }
+        
+    }
+
+    private void Disappearance()
+    {
+        Vector3 movement = Vector3.up * Time.deltaTime * disappearanceMoveSpeed;
+        gameObject.transform.position += movement;
+
+        // Also make it disappear
+        SpriteRenderer spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+        Color color = spriteRenderer.color;
+        color.a -= disappearanceTime * Time.deltaTime;
+        spriteRenderer.color = color;
+
+        spriteRenderer = background.GetComponent<SpriteRenderer>();
+        color = spriteRenderer.color;
+        color.a -= disappearanceTime * Time.deltaTime;
+        spriteRenderer.color = color;
+
+    }
+
+    private void VisualMovement()
+    {
+        // Movement
+        float curSpeed = Mathf.Sin((Time.time - creationTime) * timeCoef + ((sinTimeShift * Mathf.PI) / 180));
+        Vector3 movement = Vector3.up * sinCoef * curSpeed * Time.deltaTime;
+        gameObject.transform.position += movement;
+
+        float colorVisability = (curSpeed + 1) / 2;
+        SpriteRenderer spriteRenderer = background.GetComponent<SpriteRenderer>();
+        Color newColor = spriteRenderer.color;
+        newColor.a = colorVisability;
+        spriteRenderer.color = newColor;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && !isDisappearing)
+        {
+            PowerUpUse();
+            LateDestroy();
+        }
+    }
+
+    private void LateDestroy()
+    {
+        Destroy(this.gameObject, disappearanceTime);
+        disappearanceSpeed = 1 / disappearanceTime;
+        isDisappearing = true;
+    }
+
+    protected virtual void PowerUpUse()
+    {
+        Debug.Log("Virtual not implemented method");
+    }
+}
