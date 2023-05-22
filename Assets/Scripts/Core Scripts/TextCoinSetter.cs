3a3671665a56d8a28f054ce5979085616a58152e
s0s100] <s0s100@mail.ru>
2023-05-22 15:17:31 +0100

    Added addition/substraction coin text

--- Assets/Scripts/Core Scripts/TextCoinSetter.cs
+++ Assets/Scripts/Core Scripts/TextCoinSetter.cs
@@ -6,6 +6,9 @@ using TMPro;
 
 public class TextCoinSetter : MonoBehaviour
 {
+    [SerializeField]
+    private TMP_Text adderText;
+
     private void Awake()
     {
         UpdateCoinText();
@@ -15,5 +18,22 @@ public class TextCoinSetter : MonoBehaviour
     {
         TMP_Text textBox = GetComponent<TMP_Text>();
         textBox.text = CoinController.GetCurAmount().ToString();
+    }
+
+    public void MakeAdditionTextNotification(int coinAmount)
+    {
+        string output = "+" + coinAmount.ToString();
+        adderText.text = output;
+        Animator animator = adderText.GetComponent<Animator>();
+        animator.SetTrigger("Addition");
+    }
+
+    // Substraction actually
+    public void MakeRemovalTextNotification(int coinAmount)
+    {
+        string output = "-" + coinAmount.ToString();
+        adderText.text = output;
+        Animator animator = adderText.GetComponent<Animator>();
+        animator.SetTrigger("Removal");
     }
 }
