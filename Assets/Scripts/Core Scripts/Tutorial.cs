f34a7eab4e1baaaa55cc56e43418c72c25924c63
s0s100] <s0s100@mail.ru>
2023-06-15 13:28:46 +0100

    Created tutorial slides

--- Assets/Scripts/Core Scripts/Tutorial.cs
+++ Assets/Scripts/Core Scripts/Tutorial.cs
@@ -6,19 +6,63 @@ using UnityEngine.UI;
 public class Tutorial : MonoBehaviour
 {
     [SerializeField]
+    private Image[] frontImage;
+    [SerializeField]
+    private GameObject nextTutorial;
+    [SerializeField]
     private Button skipButton;
     [SerializeField]
-    private Image backImage;
-    [SerializeField]
-    private Image frontImage;
+    private Button nextButton;
+    
+    private int curImage = 0;
 
     private void Awake()
     {
         skipButton.onClick.AddListener(SkipTutorial);
+        nextButton.onClick.AddListener(NextHint);
+
+        if (frontImage.Length > 0)
+        {
+            Debug.Log("Showing hint number: " + curImage);
+            frontImage[0].enabled = true;
+        } else
+        {
+            Debug.Log("No image to display in tutorial!");
+            NextTutorial();
+        }
+    }
+
+    private void NextHint()
+    {
+        frontImage[curImage].enabled = false;
+        curImage++;
+        if (frontImage.Length > curImage)
+        {
+
+            Debug.Log("Showing hint number: " + curImage);
+            frontImage[curImage].enabled = true;
+        } else
+        {
+            NextTutorial();
+        }
     }
 
     private void SkipTutorial()
     {
+        Destroy(this.gameObject);
+    }
+
+    private void NextTutorial()
+    {
+        if (nextTutorial != null)
+        {
+            Instantiate(nextTutorial);
+        } else
+        {
+            Debug.Log("No other tutorial available!");
+        }
+
+        
         Destroy(this.gameObject);
     }
 }
